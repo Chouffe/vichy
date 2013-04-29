97edd6d97d642a0188186ba7694a8886eb6c290e
Romain Pomier <romain.pomier@gmail.com>
2013-04-29 18:31:10 +0200

    Loading all infos from history file

--- client/history/history.py
+++ client/history/history.py
@@ -5,9 +5,11 @@ Read a vim history file and transform it to a list
 
 import struct
 
-FILE = "test/vim_hist.txt"
+FILE = "test/%Users%pomier%projects%vichy%client%history%cuicui.txt"
 UF_ENTRY_MAGIC = 0xf518
+UF_HEADER_MAGIC = 0x5fd0
 UF_START_MAGIC = "Vim\237UnDo\345"
+NMARKS = 26
 
 class Header:
     """Represents a list of list in vim history"""
@@ -24,14 +26,21 @@ class Header:
         self.uh_visual = None
         self.entries = []
 
+    def __str__(self):
+        return str(self.entries)
+
 
 class Pos:
     """Represents the position of the header"""
 
     def __init__(self):
         self.lnum = 0
-        self.com = 0
+        self.col = 0
         self.coladd = 0
+
+    def __str__(self):
+        s = "Line %d, Col %d, Coladd %d"%(self.lnum, self.col, self.coladd)
+        return s
 
 
 class Entry:
@@ -44,6 +53,14 @@ class Entry:
         self.size = 0
         self.array = []
 
+    def __str__(self):
+        s = "Line %d to %d, %d lines, size %d"%(self.top, self.bot, self.lcount, self.size)
+        return s
+
+    def __repr__(self):
+        s = "Line %d to %d, %d lines, size %d"%(self.top, self.bot, self.lcount, self.size)
+        return s
+
 class VisualInfo:
     def __init__(self):
         self.vi_start = None
@@ -52,14 +69,52 @@ class VisualInfo:
         self.vi_curswant = 0
 
 
+class HistoryFile:
+    def __init__(self, name):
+        f = open(name, 'rb')
+        self.content = bytearray(f.read())
+        self.length = len(self.content)
+        self.pointer = 0
+
+    def read(self, n):
+        result = self.content[self.pointer:self.pointer+n]
+        self.pointer += n
+        return result
+
+    def find_next_constant(self, v1, v2):
+        while self.pointer + 1 < self.length:
+            if self.content[self.pointer] == v1 and self.content[self.pointer+1] == v2:
+                return True
+            self.pointer += 1
+        return False
+
+    def is_finished(self, n):
+        return self.pointer + n > self.length
+
+    def is_constant(self, v1, v2):
+        if self.pointer + 1 >= self.length:
+            return False
+        if self.content[self.pointer] == v1 and self.content[self.pointer+1] == v2:
+            self.pointer += 2
+            return True
+        else:
+            self.pointer += 2
+            return False
+        
+
+    
+
+def get_double(f):
+    return struct.unpack_from('d', buffer(f.read(8)[::-1]))[0]
+
 def get_int(f):
-    return struct.unpack('i', f.read(4)[::-1])[0]
+    return struct.unpack_from('i', buffer(f.read(4)[::-1]))[0]
 
 def get_short(f):
-    return struct.unpack('h', f.read(2)[::-1])[0]
+    return struct.unpack_from('H', buffer(f.read(2)[::-1]))[0]
 
 def get_char(f):
-    return struct.unpack('b', f.read(1)[::-1])[0]
+    return struct.unpack_from('b', buffer(f.read(1)[::-1]))[0]
 
 def get_header(f):
     """Get the next header of the file"""
@@ -78,20 +133,12 @@ def get_header(f):
     uhp.uh_flags = get_short(f)
 
     uhp.uh_visual = get_visual(f)
-    # ctime
-    f.read(8)
 
-    while True:
-        length = get_int(f)
-        if length == 0:
-            break
-        what = get_char(f)
-        if what == 1:
-            f.read(length)
-
-    while get_short(f) == UF_ENTRY_MAGIC:
+    f.find_next_constant(245, 24)
+    while f.is_constant(245, 24):
         uep = get_entry(f)
         uhp.entries.append(uep)
+
 
     return uhp
 
@@ -119,8 +166,8 @@ def get_visual(f):
     v = VisualInfo()
     v.vi_start = get_pos(f)
     v.vi_end = get_pos(f)
-    v.vi_mode = get_int()
-    v.vi_curswant = get_int()
+    v.vi_mode = get_int(f)
+    v.vi_curswant = get_int(f)
     return v
 
 def read_header(f):
@@ -141,10 +188,9 @@ def read_header(f):
     str_len = get_int(f)
     if str_len > 0:
         s = f.read(str_len)
-        print "String: %s"%(s)
         ln = get_int(f)
         cn = get_int(f)
-        print "%d %d"%(ln,cn)
+        print "Init text:\n%s"%(s)
 
     old_header_seq = get_int(f);
     new_header_seq = get_int(f);
@@ -152,10 +198,24 @@ def read_header(f):
     num_head = get_int(f);
     seq_last = get_int(f);
     seq_cur = get_int(f);
-    # Time
-    seq_time = f.read(8);
+
+    headers = []
+    while f.find_next_constant(95, 208):
+        f.is_constant(95, 208)
+        print "\n-------\nNew header\n-------\n"
+        uhp = get_header(f)
+        k = 0
+        for i in uhp.entries:
+            print "Entry %d"%(k)
+            print i
+            for j in i.array:
+                print ">>>> %s"%(j)
+            k += 1
+            print ""
+        headers.append(uhp)
+
 
 
 if __name__ == '__main__':
-    f = open(FILE, 'rb')
+    f = HistoryFile(FILE)
     read_header(f)
