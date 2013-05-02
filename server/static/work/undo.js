87879afe86fe3d2a524085d701d8fdabe7842505
Caroline Keramsi <caroline.keramsi@gmail.com>
2013-05-02 16:19:20 +0200

    Entering "Undo handler"

--- server/static/work/undo.js
+++ server/static/work/undo.js
@@ -1,4 +1,16 @@
 function undo()
 {
+    var xhr = new XMLHttpRequest();
+    var param = encodeURIComponent("machin");
+    var machin = 3;
+    var truc = 4;
+    xhr.open("POST", "http://localhost:8000/undo", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("param="+machin+"&param2="+truc);
+    
+    xhr.onreadystatechange = function() {
+    };
+
     alert("Undo");
 }
+
