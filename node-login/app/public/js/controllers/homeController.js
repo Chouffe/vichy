6b55111f70b3d5d5c7021c48d687315d37082642
paullagree <paul.lagree@gmail.com>
2013-05-06 11:12:55 +0200

    Add node-login module

---
+++ node-login/app/public/js/controllers/homeController.js
@@ -0,0 +1,91 @@
+
+function HomeController()
+{
+
+// bind event listeners to button clicks //
+	var that = this;
+
+// handle user logout //
+	$('#btn-logout').click(function(){ that.attemptLogout(); });
+
+// confirm account deletion //
+	$('#account-form-btn1').click(function(){console.log($('.modal-confirm')); $('.modal-confirm').modal('show');});
+	
+// open My documents
+	$('#account-form-btn3').click(function(){console.log($('.modal-mydocs')); $('.modal-mydocs').modal('show');});
+
+// handle account deletion //
+	$('.modal-confirm .submit').click(function(){ that.deleteAccount(); });
+	
+// handle documents change //
+
+	$('.modal-mydocs .submit').click(function(){ that.addUser(); });
+	
+	this.addUser = function()
+	{
+		$('.modal-mydocs').modal('hide');
+		var that = this;
+		$.ajax({
+			url: '/mydocuments',
+			type: 'POST',
+			data: { id: $('#userId').val(), },
+			success: function(data){
+	 			that.showLockedAlert('Your account has been deleted.<br>Redirecting you back to the homepage.');
+			},
+			error: function(jqXHR){
+				console.log(jqXHR.responseText+' :: '+jqXHR.statusText);
+			}
+		});
+	}
+
+	this.deleteAccount = function()
+	{
+		$('.modal-confirm').modal('hide');
+		var that = this;
+		$.ajax({
+			url: '/delete',
+			type: 'POST',
+			data: { id: $('#userId').val()},
+			success: function(data){
+	 			that.showLockedAlert('Your account has been deleted.<br>Redirecting you back to the homepage.');
+			},
+			error: function(jqXHR){
+				console.log(jqXHR.responseText+' :: '+jqXHR.statusText);
+			}
+		});
+	}
+
+	this.attemptLogout = function()
+	{
+		var that = this;
+		$.ajax({
+			url: "/home",
+			type: "POST",
+			data: {logout : true},
+			success: function(data){
+	 			that.showLockedAlert('You are now logged out.<br>Redirecting you back to the homepage.');
+			},
+			error: function(jqXHR){
+				console.log(jqXHR.responseText+' :: '+jqXHR.statusText);
+			}
+		});
+	}
+
+	this.showLockedAlert = function(msg){
+		$('.modal-alert').modal({ show : false, keyboard : false, backdrop : 'static' });
+		$('.modal-alert .modal-header h3').text('Success!');
+		$('.modal-alert .modal-body p').html(msg);
+		$('.modal-alert').modal('show');
+		$('.modal-alert button').click(function(){window.location.href = '/';})
+		setTimeout(function(){window.location.href = '/';}, 3000);
+	}
+}
+
+HomeController.prototype.onUpdateSuccess = function()
+{
+	$('.modal-alert').modal({ show : false, keyboard : true, backdrop : true });
+	$('.modal-alert .modal-header h3').text('Success!');
+	$('.modal-alert .modal-body p').html('Your account has been updated.');
+	$('.modal-alert').modal('show');
+	$('.modal-alert button').off('click');
+}
