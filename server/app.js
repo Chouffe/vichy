fbe380a98d6d0857aa97096622a2fe08c7190003
Caroline Keramsi <caroline.keramsi@gmail.com>
2013-05-07 11:27:28 +0200

    Merge work on shareJs with work on node-login

--- server/app.js
+++ server/app.js
@@ -9,24 +9,49 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
+var undo = require('./lib/undo');
+ShareJS = require('share').server;
+
+
+ShareJSOpts = {
+  browserChannel: {
+    cors: "*"
+  },
+  db: {
+    //type: "none"
+    type: "mongo",
+    opsCollectionPerDoc: false
+  }
+};
+
+
 app.configure(function(){
 	app.set('port', 8088);
 	app.set('views', __dirname + '/app/server/views');
 	app.set('view engine', 'jade');
 	app.locals.pretty = true;
 //	app.use(express.favicon());
-//	app.use(express.logger('dev'));
+  app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.cookieParser());
 	app.use(express.session({ secret: 'super-duper-secret-secret' }));
 	app.use(express.methodOverride());
 	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
 	app.use(express.static(__dirname + '/app/public'));
+  app.use(function logerror(err, req, res, next)
+     {
+       console.log(err.stack);
+       console.log(req);
+       next();
+     });
 });
 
 app.configure('development', function(){
 	app.use(express.errorHandler());
 });
+
+ShareJS.attach(app, ShareJSOpts);
+undo.setModel(app.model);
 
 require('./app/server/router')(app);
 
