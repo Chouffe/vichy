1041221aa9381b7986583ebc3a2805685cbe1d78
Caroline Keramsi <caroline.keramsi@gmail.com>
2013-05-10 19:31:34 +0200

    FIX: undo after implementation of openDoc

--- server/app/public/static/work/undo.js
+++ server/app/public/static/work/undo.js
@@ -1,7 +1,7 @@
 function undo()
 {
 
-    var connection = sharejs.open(docToOpen, 'text', function(error, doc)
+    var connection = sharejs.open(documentToOpen, 'text', function(error, doc)
     {
         // this function is called once the connection is opened
         if (error) {
@@ -9,7 +9,7 @@ function undo()
         } else {
             // attach the ShareJS document to the textarea
             var xhr = new XMLHttpRequest();
-            var doc_name = encodeURIComponent(docToOpen);
+            var doc_name = encodeURIComponent(documentToOpen);
             var version = doc.version;
             xhr.open("POST", "http://localhost:8088/undo", true);
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
